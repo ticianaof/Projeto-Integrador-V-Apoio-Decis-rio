28818ec226b1228f49be914fdb653bb8a4d613d7
almeidavnnc <38260501+almeidavnnc@users.noreply.github.com>
2026-03-09 12:35:41 -0300

    Update and rename DDL_create_bd.sql to SCRIPTS.sql

--- SCRIPTS.sql
+++ SCRIPTS.sql
@@ -40,4 +40,21 @@ CREATE TABLE dw_neet.fato_indicadores_neet (
     taxa_neet DECIMAL(5,2),
     escolaridade_media DECIMAL(5,2),
     pib_per_capita DECIMAL(15,2)
+
+-- Scripts DML
+
+-- Carga em Lot
+-- Utilizando os arquivos CSV gerados após a limpeza e transformação em Python
+
+COPY dw_neet.dim_localidade (sigla_uf, nome_estado, regiao)
+FROM '/caminho_do_projeto/dados_transformados/dim_localidade.csv' DELIMITER ',' CSV HEADER;
+
+COPY dw_neet.dim_localidade (sigla_uf, nome_estado, regiao)
+FROM '/caminho_do_repositorio/dados_limpos/dim_localidade.csv' DELIMITER ',' CSV HEADER;
+
+COPY dw_neet.dim_perfil (faixa_etaria, sexo)
+FROM '/caminho_do_repositorio/dados_limpos/dim_perfil.csv' DELIMITER ',' CSV HEADER;
+
+COPY dw_neet.fato_indicadores_neet (id_localidade, id_perfil, id_tempo, total_jovens_populacao, total_jovens_neet, taxa_neet, escolaridade_media, pib_per_capita)
+FROM '/caminho_do_repositorio/dados_limpos/fato_neet.csv' DELIMITER ',' CSV HEADER;
 );
